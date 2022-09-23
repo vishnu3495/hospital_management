179d35a61bbbcba92480a88db0a0deeb4d5128bd
Vishnu <i_vishnu.r@ults.in>
2022-09-23 11:41:29 +0530

    modified

--- hospital_management/custom_addons/om_hospital/models/doctors.py
+++ hospital_management/custom_addons/om_hospital/models/doctors.py
@@ -1,19 +1,18 @@
-from odoo import models,fields
+from odoo import models, fields
 
 
 class HospitalDoctors(models.Model):
     # _inherit = 'hospital.patient'
-    _name="hospital.doctors"
+    _name = "hospital.doctors"
     _description = "Hospital Doctors"
     _rec_name = "doctor_name"
 
-    doctor_name=fields.Char(string='Doctor Name')
+    doctor_name = fields.Char(string='Doctor Name')
     age = fields.Integer(string='Age')
-    place=fields.Char(string='Place')
-    specialise=fields.Char(string='Specialisation')
+    place = fields.Char(string='Place')
+    specialise = fields.Char(string='Specialisation')
 
-    patient_ids=fields.One2many('hospital.patient','doctor_detail_id',string='Patient')
+    patient_ids = fields.One2many('hospital.patient', 'doctor_detail_id', string='Patient')
 
 
-   # gender=fields.Selection(selection_add=[('other','other')])
-
+# gender = fields.Selection(selection_add=[('other', 'other')])
